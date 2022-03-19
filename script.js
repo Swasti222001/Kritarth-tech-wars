aad6d6871fb75c9cea8abd859c7963ac5d20a425
Swasti <swastiofficial2001@gmail.com>
2022-03-20 03:31:17 +0530

    Nav Bar

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ $(".item_one").click(function(){
   $(".hamburger_item").toggleClass("item_none")
   // console.log(1);
 })
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+
+var tl = gsap.timeline({ repeat: -1 });
+tl.to("h1", 30, { backgroundPosition: "-960px 0" });
